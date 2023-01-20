75833af367245cbb0f41149cb7a1bf0e4b134054
Superdegot <65909405+Superdegot@users.noreply.github.com>
2023-01-20 16:23:54 +0000

    Added link to dictionary app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
 import Navbar from './Navbar';
+
+
 function App() {
   return (
     <div className="App">
-
-< Navbar />
+      <Navbar />
+      <h2 className="text-center">👋 Hi, I am</h2>
+      <h1 className="text-center">Ekaterina Filatova</h1>
+      <h3 className="text-center">
+        Junior Front-end developer, based in London, UK
+      </h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <img
+              src="#"
+              className="img-fluid rounded d-none d-sm-block"
+              alt="Dictionary screenshot"
+            />
+          </div>
+          <div class="col-sm-6">
+            <h2 class="mb-3 ">Dictionary App</h2>
+            <p>My Dictionary project</p>
+            <div class="mt-5">
+              <a
+                href="https://mellow-biscotti-bc44e5.netlify.app/"
+                class="primary-link project-link"
+              >
+                Launch Dictionary App
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
